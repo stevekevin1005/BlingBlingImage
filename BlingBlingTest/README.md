7355e3ee83b7aff810f7712b4830ca80a310eb24
qqwwee2006@gmail.com <qqwwee2006@gmail.com>
2015-07-10 03:20:57 +0800

    Revert "圖片閃爍"
    This reverts commit 2f7656b27a71c7661617aae07758e5cdead1a22d.

--- BlingBlingTest/README.md
+++
@@ -1,8 +0,0 @@
-# 利用Timer 達到圖片閃爍出現在不同位置效果
-
-注意:
-timer 不能在UI thread內跑
-
-設定圖片用 LayoutParams
-Params 有點類似 父與子的關係(請google)
-使用上必須先看清楚 上層的layout是哪款
--- BlingBlingTest/gen/com/example/blingblingtest/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.example.blingblingtest;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
